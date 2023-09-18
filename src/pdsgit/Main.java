ce5eab8b949999e1a5c5291be13d831c64a53b42
Daniel Morais <danielmorais@outlook.com.br>
2023-09-18 09:54:56 -0300

    hello 6

--- src/pdsgit/Main.java
+++ src/pdsgit/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Hello World 2");
 		System.out.println("Hello World 3");
 		System.out.println("Hello World 5");
+		System.out.println("Hello World 6");
 
 	}
 
